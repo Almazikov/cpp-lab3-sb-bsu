e04259b1707321c652cb6869896f626c3f4c5042
Almazikov <krot643@gmail.com>
2024-11-25 21:43:54 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -5,6 +5,14 @@ int main()
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
+for (int n = 0; n <= 1;)
+{
+  cout << "Error!!! The amount of integers to sum must be more than 0, enter the amount of integers to sum = "; cin >> k;
+if (k > 0)
+ {
+break;
+ }
+};
 do {
 cout << "Enter integer nr. " << i << ": "; 
 cin >> number;
